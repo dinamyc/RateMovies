b4059bbd63bba1670f4d3db0dbd366d5aa27832b
JL <j.luis.enriquez77@gmail.com>
2024-04-23 17:32:29 -0600

    Initial Commit

---
+++ RateMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RateMovies
+//
+//  Created by JL on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
